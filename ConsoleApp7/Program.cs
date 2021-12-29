d6d76709eb5a7990bbce788a6cad86bf7a039bde
PC <PC@HANEEN>
2021-12-30 00:41:58 +0200

    Add project files.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        //public static int SquareSum(int[] n)
+        //{
+        //    n = new int[5];
+
+        //    Console.WriteLine("please input numbers");
+        //    for (int i = 0; i < n.Length; i++)
+        //    {
+        //        n[i] = Convert.ToInt32(Console.ReadLine());
+        //        Math.Pow(n[i], 2);
+        //        int sum = 0;
+        //        sum = sum + n[i];
+
+        //        Console.WriteLine(sum);
+
+
+
+        //    }
+        //    return SquareSum(n);
+
+        //}
+
+
+
+
+
+
+
+
+
+
+
+        //public static string EvenOrOdd(int number)
+        //{
+        //        if (number % 2 == 0)
+        //        {
+        //        Console.WriteLine("even");
+        //        return 
+        //            ("even");
+
+        //        }
+
+
+        //        else
+        //        {
+        //        Console.WriteLine("odd");
+        //        return ("odd");
+
+
+        //        }
+
+
+
+
+
+
+        //public static bool IsPangram(string str)
+        //{
+        //string[] arr = str.Split(new char[] { , });
+
+        //A pangram is a sentence that contains every single letter of the alphabet at least once.For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once(case is irrelevant).
+
+        //Given a string, detect whether or not it is a pangram. Return True if it is, False if not.Ignore numbers and punctuation.
+
+
+
+
+
+
+
+        //public static int Opposite(int number)
+        //{
+
+        //    return -number;
+        //}
+
+        //public static bool IsIsogram(string str)
+        //{
+
+        //char [] chars = str.ToLower().ToCharArray();
+
+        //    for (int i = 0; i < chars.Length; i++)
+        //    {
+        //        for (int j = i + 1; j < chars.Length; j++)
+
+
+        //            if (chars[i] == chars[j]) {
+        //                return false;
+        //            }
+        //    }
+
+        //     return true;   
+
+
+        //}
+
+
+
+
+
+
+
+
+
+
+        //public static bool IsSquare(int n)
+        //{
+        //    //Your code goes here!
+        //    int num = (int)Math.Sqrt(n);
+        //  return (int)Math.Pow(num, 2) == n;
+
+        //}
+        // return Math.Sqrt(n) % 1 == 0;
+
+
+
+
+
+
+
+
+
+        //public static string GetMiddle(string s) { 
+
+        //    int str_lenght = s.Length;
+
+        //        if (str_lenght % 2 != 0)
+        //        {
+        //          return s.Substring(str_lenght/ 2 , 1);
+
+
+        //        }
+        //        else
+        //        {
+        //           return s.Substring(str_lenght / 2- 1, 2);
+
+
+        //        }
+
+        //    }
+
+
+
+
+
+
+
+        //public static string Remove_char(string s)
+        //{
+        //    // Your Code
+
+        //    int str_lenght = s.Length-2;
+
+        //    return s.Substring(1, str_lenght);
+
+
+        //public static string Likes(string[] name)
+        //{
+        //    string[][] Name = new string[][]{
+
+        //   new string[]{ } ,
+        //   new string[] { "Peter" },
+        //   new string[] { "Jacob", "Alex" },
+        //   new string[] { "Max", "John", "Mark" },
+        //   new string[] { "Alex", "Jacob" , "Mark", "Max"},
+
+        //    };
+
+
+        //    if (name.Length == 0)
+        //    {
+        //        return "no one likes this";
+        //    }
+
+        //    else if (name.Length == 1)
+        //    {
+        //        return Name[1][0] + " like this";
+        //    }
+
+        //    else if (name.Length == 2)
+        //    {
+        //        return Name[2][0] + " and " + Name[2][1] + " like this";
+        //    }
+
+
+        //    else if (name.Length == 3)
+        //    {
+        //        return Name[3][0] + ", " + Name[3][1] + " and " + Name[3][2] + " like this";
+        //    }
+
+        //    else if (name.Length >= 4)
+        //    {
+        //        int n = name.Length - 2;
+        //        return Name[4][0] + " , " + Name[4][1] +" and" + $" {n} others like this";
+
+        //    }
+        //    else
+        //    {
+        //        throw new NotImplementedException();
+        //    }
+
+        //}
+
+
+        
+            static void Main(string[] args)
+                {
+
+            
+
+                }
+            }
+
+        }
+    
+ 
+
+    
+
+
